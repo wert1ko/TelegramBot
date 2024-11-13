4a6e39c638f165880967eb77ca32e16ededc7b9a
CodeCrafted1 <novikknow77@gmail.com>
2024-11-13 11:51:02 +0200

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from schemas.message_data import MessageData
 from aiogram import Bot
+from fastapi.middleware.cors import CORSMiddleware
 from config import settings
 
 app = FastAPI()
 bot = Bot(token=settings.bot.BOT_TOKEN)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/bot/send_to_channel")
